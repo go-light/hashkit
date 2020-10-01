2e5969dc56c8ca2a01b99068af712b65ee74fb06
qinyif <qinyifeng@outlook.com>
2020-10-01 22:00:09 +0800

    update func name

--- fnv_test.go
+++ fnv_test.go
@@ -4,30 +4,30 @@ import "testing"
 
 const str = "abcdefghijklmnopqrstuvwxyz0123456789"
 
-func Benchmark_hash_fnv1_32(b *testing.B) {
+func Benchmark_Fnv32(b *testing.B) {
 	b.ResetTimer()
 	for i := 0; i < b.N; i++ {
-		Hash_fnv1_32(str)
+		Fnv32(str)
 	}
 }
 
-func Benchmark_hash_fnv1a_32(b *testing.B) {
+func Benchmark_Fnv32a(b *testing.B) {
 	b.ResetTimer()
 	for i := 0; i < b.N; i++ {
-		Hash_fnv1a_32(str)
+		Fnv32a(str)
 	}
 }
 
-func Benchmark_hash_fnv1_64(b *testing.B) {
+func Benchmark_Fnv64(b *testing.B) {
 	b.ResetTimer()
 	for i := 0; i < b.N; i++ {
-		Hash_fnv1_64(str)
+		Fnv64(str)
 	}
 }
 
-func Benchmark_hash_fnv1a_64(b *testing.B) {
+func Benchmark_Fnv64a(b *testing.B) {
 	b.ResetTimer()
 	for i := 0; i < b.N; i++ {
-		Hash_fnv1a_64(str)
+		Fnv64a(str)
 	}
 }
